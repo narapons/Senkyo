1f1823a19ee7bead54c4daff7735bd008320a0e9
narapon <43005911+narapons@users.noreply.github.com>
2021-03-08 01:47:53 +0900

    ソースコードのお掃除

--- src/senkyo/main.php
+++ src/senkyo/main.php
@@ -12,73 +12,73 @@ use pocketmine\Player;
 class main extends PluginBase implements Listener
 {
 
-	public function onEnable()
-	{
-		$this->senkyo = new Config($this->getDataFolder() . "senkyo.yml", Config::YAML);
-		$this->player = new Config($this->getDataFolder() . "player.yml", Config::YAML);
-		$this->setting = new Config($this->getDataFolder() . "setting.yml", Config::YAML);
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
+    public function onEnable()
+    {
+        $this->senkyo = new Config($this->getDataFolder() . "senkyo.yml", Config::YAML);
+        $this->player = new Config($this->getDataFolder() . "player.yml", Config::YAML);
+        $this->setting = new Config($this->getDataFolder() . "setting.yml", Config::YAML);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
 
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
-	{
-		switch ($command->getName()) {
-			case"senkyo":
-				if (!isset($args[0])) {
-					if ($sender->isOp()) {
-						$sender->sendMessage("§e[選挙システム] /senkyo [ add / remove / list / vote / setting ]");
-					} else {
-						$sender->sendMessage("§e[選挙システム] /senkyo list で政党の一覧と投票数を確認できます。");
-						$sender->sendMessage("§e[選挙システム] /senkyo vote [政党名] で指定した政党に投票できます。");
-					}
-				} else {
-					switch ($args[0]) {
-						case"add":
-							if (!$sender->isOp()) {
-								$sender->sendMessage("§cこのコマンドを実行する権限がありません。");
-							} else if (!isset($args[1])) {
-							    $sender->sendMessage("§e[選挙システム] 追加をする政党名を入力してください。");
-							} else if ($this->senkyo->exists($args[1])) {
-							    $sender->sendMessage("§e[選挙システム] 「{$args[1]}」は既に追加されています。");
-							} else {
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        switch ($command->getName()) {
+            case"senkyo":
+                if (!isset($args[0])) {
+                    if ($sender->isOp()) {
+                        $sender->sendMessage("§e[選挙システム] /senkyo [ add / remove / list / vote / setting ]");
+                    } else {
+                        $sender->sendMessage("§e[選挙システム] /senkyo list で政党の一覧と投票数を確認できます。");
+                        $sender->sendMessage("§e[選挙システム] /senkyo vote [政党名] で指定した政党に投票できます。");
+                    }
+                } else {
+                    switch ($args[0]) {
+                        case"add":
+                            if (!$sender->isOp()) {
+                                $sender->sendMessage("§cこのコマンドを実行する権限がありません。");
+                            } else if (!isset($args[1])) {
+                                $sender->sendMessage("§e[選挙システム] 追加をする政党名を入力してください。");
+                            } else if ($this->senkyo->exists($args[1])) {
+                                $sender->sendMessage("§e[選挙システム] 「{$args[1]}」は既に追加されています。");
+                            } else {
                                 $this->senkyo->set($args[1], 0);
                                 $this->senkyo->save();
                                 $this->senkyo->reload();
                                 $sender->sendMessage("§6[選挙システム] 「{$args[1]}」を追加しました。");
                             }
-							break;
-						case"remove":
-							if (!$sender->isOp()) {
-								$sender->sendMessage("§cこのコマンドを実行する権限がありません。");
-							} else if (!isset($args[1])) {
-							    $sender->sendMessage("§e[選挙システム] 削除をする政党名を入力してください。");
-							} else if (!$this->senkyo->exists($args[1])) {
-							    $sender->sendMessage("§e[選挙システム] 「{$args[1]}」は存在しません。");
-							} else {
+                            break;
+                        case"remove":
+                            if (!$sender->isOp()) {
+                                $sender->sendMessage("§cこのコマンドを実行する権限がありません。");
+                            } else if (!isset($args[1])) {
+                                $sender->sendMessage("§e[選挙システム] 削除をする政党名を入力してください。");
+                            } else if (!$this->senkyo->exists($args[1])) {
+                                $sender->sendMessage("§e[選挙システム] 「{$args[1]}」は存在しません。");
+                            } else {
                                 $this->senkyo->remove($args[1]);
                                 $this->senkyo->save();
                                 $this->senkyo->reload();
                                 $sender->sendMessage("§6[選挙システム] 「{$args[1]}」を削除しました。");
                             }
-							break;
-						case"list":
-							$sender->sendMessage("§6===選挙システム===");
-							foreach($this->senkyo->getAll() as $key=>$value){
-								$sender->sendMessage("政党：{$key}   投票数{$value}");
-							}
-							break;
-						case"vote":
-						    $name = $sender->getName();
-						    if (!$this->setting->exists("on")) {
-						        $sender->sendMessage("§e[選挙システム] 現在は選挙が行われていません。");
+                            break;
+                        case"list":
+                            $sender->sendMessage("§6===選挙システム===");
+                            foreach ($this->senkyo->getAll() as $key => $value) {
+                                $sender->sendMessage("政党：{$key}   投票数{$value}");
+                            }
+                            break;
+                        case"vote":
+                            $name = $sender->getName();
+                            if (!$this->setting->exists("on")) {
+                                $sender->sendMessage("§e[選挙システム] 現在は選挙が行われていません。");
                             } else if (!isset($args[1])) {
-								$sender->sendMessage("§e[選挙システム] 投票する政党を入力してください。");
+                                $sender->sendMessage("§e[選挙システム] 投票する政党を入力してください。");
                             } else if (!$this->senkyo->exists($args[1])) {
                                 $sender->sendMessage("§e[選挙システム] 「{$args[1]}」は存在しません。");
-							} else if ($this->player->exists($name)) {
-							    $data = $this->player->get($name);
-							    $sender->sendMessage("§e[選挙システム] 既にあなたは「{$data}」に投票をしています。");
-							} else {
+                            } else if ($this->player->exists($name)) {
+                                $data = $this->player->get($name);
+                                $sender->sendMessage("§e[選挙システム] 既にあなたは「{$data}」に投票をしています。");
+                            } else {
                                 $data = $this->senkyo->get($args[1]) + 1;
                                 $this->senkyo->set($args[1], $data);
                                 $this->senkyo->save();
@@ -88,13 +88,13 @@ class main extends PluginBase implements Listener
                                 $this->player->reload();
                                 $sender->sendMessage("§6[選挙システム] 「{$args[1]}」に投票しました。");
                             }
-							break;
-						case"setting":
-							if (!$sender->isOp()) {
-								$sender->sendMessage("§cこのコマンドを実行する権限はありません。");
-							} else if (!isset($args[1])) {
-									$sender->sendMessage("§e[選挙システム] 使い方 /senkyo setting [on / off / now]");
-							} else {
+                            break;
+                        case"setting":
+                            if (!$sender->isOp()) {
+                                $sender->sendMessage("§cこのコマンドを実行する権限はありません。");
+                            } else if (!isset($args[1])) {
+                                $sender->sendMessage("§e[選挙システム] 使い方 /senkyo setting [on / off / now]");
+                            } else {
                                 switch ($args[1]) {
                                     case"on":
                                         if ($this->setting->exists("on")) {
@@ -128,19 +128,18 @@ class main extends PluginBase implements Listener
                                         break;
                                 }
                             }
-							break;
-						default:
-							if($sender->isOp()) {
-								$sender->sendMessage("§e[選挙システム] /senkyo [ add / remove / list / vote / setting ]");
-							}else{
-								$sender->sendMessage("§e[選挙システム] /senkyo list で政党の一覧と投票数を確認できます。");
-								$sender->sendMessage("§e[選挙システム] /senkyo vote [政党名] で指定した政党に投票できます。");
-							}
-							break;
-					}
-				}
-		}
-		return true;
-	}
-
+                            break;
+                        default:
+                            if ($sender->isOp()) {
+                                $sender->sendMessage("§e[選挙システム] /senkyo [ add / remove / list / vote / setting ]");
+                            } else {
+                                $sender->sendMessage("§e[選挙システム] /senkyo list で政党の一覧と投票数を確認できます。");
+                                $sender->sendMessage("§e[選挙システム] /senkyo vote [政党名] で指定した政党に投票できます。");
+                            }
+                            break;
+                    }
+                }
+        }
+        return true;
+    }
 }
